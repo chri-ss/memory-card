15a4aa8663d0ea66696d95cc5f13035597754b58
Chris Cresswell <Christorper@gmail.com>
2022-09-14 21:00:15 -0700

    Dispay cards randomly and shuffle on click

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import CardContainer from "./components/CardContainer";
 import cards from "./images/cardLoader";
 function App() {
   const [allCards, setAllCards] = useState(cards);
 
+  //mutable, make sure to copy arr in state and reassign afterwards.
+  const shuffle = (arr) => {
+    let curr = arr.length - 1;
+    while (curr !== 0) {
+      let swap = Math.floor(Math.random() * curr);
+      curr--;
+
+      let temp = arr[curr];
+      arr[curr] = arr[swap];
+      arr[swap] = temp;
+    }
+    return arr;
+  };
+
+  const shuffleCards = () => {
+    const tempCards = [...allCards];
+    shuffle(tempCards);
+    setAllCards(tempCards);
+  };
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
   return (
     <div className="flex flex-col App h-screen">
       <Header />
-      <CardContainer allCards={allCards} />
+      <CardContainer allCards={allCards} shuffleCards={shuffleCards} />
     </div>
   );
 }
--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,13 +1,17 @@
 import uniqid from "uniqid";
 
-const CardContainer = ({ allCards }) => {
+const CardContainer = ({ allCards, shuffleCards }) => {
   const cardsToShow = allCards.filter((card, i) => i > 16);
   return (
     <div className="flex w-full h-full justify-center items-center">
       <ul className="grid h-5/6 w-5/6 border border-black grid-cols-4 grid-rows-4 gap-2 justify-items-center">
         {cardsToShow.map((card) => (
           <li key={uniqid()}>
-            <img className="h-full" src={card.image}></img>
+            <img
+              className="h-full"
+              src={card.image}
+              onClick={shuffleCards}
+            ></img>
           </li>
         ))}
       </ul>
