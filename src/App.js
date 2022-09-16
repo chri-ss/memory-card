2140079d5970de0f2377b212a024f2142da58aa0
Chris Cresswell <Christorper@gmail.com>
2022-09-15 23:00:38 -0700

    Set seen property to true on card when clicked. Shuffle function moved to useeffect.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CardContainer from "./components/CardContainer";
 import cards from "./images/cardLoader";
 function App() {
   const [allCards, setAllCards] = useState(cards);
+  const [round, setRound] = useState(0);
 
   //mutable, make sure to copy arr in state and reassign afterwards.
   const shuffle = (arr) => {
@@ -19,6 +20,21 @@ function App() {
     return arr;
   };
 
+  const markSeen = (e) => {
+    setAllCards(
+      allCards.map((card) => {
+        if (card.id === e.target.id) {
+          return { ...card, seen: true };
+        } else {
+          return card;
+        }
+      })
+    );
+    // shuffleCards();
+    setRound(round + 1);
+    console.log(allCards);
+  };
+
   const shuffleCards = () => {
     const tempCards = [...allCards];
     shuffle(tempCards);
@@ -27,12 +43,16 @@ function App() {
 
   useEffect(() => {
     shuffleCards();
-  }, []);
+  }, [round]);
 
   return (
     <div className="flex flex-col App h-screen">
       <Header />
-      <CardContainer allCards={allCards} shuffleCards={shuffleCards} />
+      <CardContainer
+        allCards={allCards}
+        // shuffleCards={shuffleCards}
+        markSeen={markSeen}
+      />
     </div>
   );
 }
--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,16 +1,16 @@
-import uniqid from "uniqid";
-
-const CardContainer = ({ allCards, shuffleCards }) => {
+const CardContainer = ({ allCards, markSeen }) => {
   const cardsToShow = allCards.filter((card, i) => i > 16);
   return (
     <div className="flex w-full h-full justify-center items-center">
       <ul className="grid h-5/6 w-5/6 border border-black grid-cols-4 grid-rows-4 gap-2 justify-items-center">
         {cardsToShow.map((card) => (
-          <li key={uniqid()}>
+          <li key={card.id}>
             <img
               className="h-full"
+              alt="hanafuda-card"
               src={card.image}
-              onClick={shuffleCards}
+              id={card.id}
+              onClick={markSeen}
             ></img>
           </li>
         ))}
