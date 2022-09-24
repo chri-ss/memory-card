bdadeebf3498c9fb818cdf9fe4f7df5aedbe6a66
Chris Cresswell <Christorper@gmail.com>
2022-09-24 01:42:26 -0700

    Add more images and combos to rules

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,9 +4,13 @@ const Modal = ({ about, rules, toggleAbout, toggleRules }) => {
   //cards for rules modal
   const plants = cards.filter((card) => card.type === "kasu");
   const ribbons = cards.filter((card) => card.type === "tanzaku");
+  const animals = cards.filter((card) => card.type === "tane");
+  const specials = cards.filter((card) => card.type === "hikari");
+
+  const aprils = cards.filter((card) => card.month === "april");
 
   return about === true || rules === true ? (
-    <div className=" flex w-screen h-screen absolute justify-center items-center text-lg text-center flex-col">
+    <div className=" flex w-screen h-screen absolute justify-center items-center text-2xl text-center flex-col">
       <div className=" w-full h-full opacity-50 bg-gray-300"></div>
       <div
         className="absolute top-28 right-1/4 cursor-pointer text-5xl"
@@ -37,12 +41,19 @@ const Modal = ({ about, rules, toggleAbout, toggleRules }) => {
             ensures there is always at least one card that you haven't seen yet,
             but the difficulty increases quickly as you click.
           </p>
+          <br></br>
+          <a
+            href="https://en.wikipedia.org/wiki/Hanafuda"
+            target="_blank"
+            rel="noreferrer"
+            className="text-cyan-900 hover:text-cyan-300 bg-zinc-400 hover:bg-zinc-700 p-1 border border-zinc-800 rounded-sm"
+          >
+            Hanafuda
+          </a>
         </div>
       ) : (
-        <div className=" flex w-6/12 h-4/6 p-6 z-100 bg-white absolute overflow-auto border border-black rounded-md flex-col items-center">
-          <h2 className="flex text-4xl justify-center indent-0 underline">
-            Objective
-          </h2>
+        <div className=" flex sm:w-4/6 lg:w-2/6 h-4/6 p-6 z-100 bg-white absolute overflow-auto border border-black rounded-md flex-col items-center">
+          <h2 className="flex text-4xl justify-center underline">Objective</h2>
           <br></br>
           <p>
             Click cards to score points. Don't click the same card twice!
@@ -50,9 +61,7 @@ const Modal = ({ about, rules, toggleAbout, toggleRules }) => {
             your score. See below for more.
           </p>
           <br></br>
-          <h3 className="flex text-4xl justify-center indent-0 underline">
-            Scoring
-          </h3>
+          <h3 className="flex text-4xl justify-center underline">Scoring</h3>
           <br></br>
           <p>Plant (kasu) cards - 1 point</p>
           <br></br>
@@ -68,9 +77,10 @@ const Modal = ({ about, rules, toggleAbout, toggleRules }) => {
             ))}
           </ul>
           <br></br>
-          <p>Ribbon (tazaku) cards - 5 points</p>
           <br></br>
-          <ul className="grid h-min w-5/6 grid-cols-6 grid-rows-4 gap-2">
+          <p>Ribbon (tanzaku) cards - 5 points</p>
+          <br></br>
+          <ul className="grid h-min w-4/6 grid-cols-5 grid-rows-2 gap-2">
             {ribbons.map((ribbon) => (
               <li className="" key={ribbon.id}>
                 <img
@@ -81,6 +91,96 @@ const Modal = ({ about, rules, toggleAbout, toggleRules }) => {
               </li>
             ))}
           </ul>
+          <br></br>
+          <br></br>
+          <p>Animal* (tane) cards - 10 points</p>
+          <br></br>
+          <p className="text-xs w-8/12">
+            *note: also included are the '8-plank bridge' and 'sake cup' cards,
+            despite not being animals{" "}
+          </p>
+          <br></br>
+          <ul className="grid h-min w-4/6 grid-cols-5 grid-rows-2 gap-2">
+            {animals.map((animal) => (
+              <li className="" key={animal.id}>
+                <img
+                  className="h-auto w-auto"
+                  alt="hanafuda-card"
+                  src={animal.image}
+                ></img>
+              </li>
+            ))}
+          </ul>
+          <br></br>
+          <br></br>
+          <p>Special (hikari, 'bright') cards - 20 points</p>
+          <br></br>
+          <ul className="grid h-min w-4/6 grid-cols-5 gap-2">
+            {specials.map((special) => (
+              <li className="" key={special.id}>
+                <img
+                  className="h-auto w-auto"
+                  alt="hanafuda-card"
+                  src={special.image}
+                ></img>
+              </li>
+            ))}
+          </ul>
+          <br></br>
+          <br></br>
+          <div className="flex w-5/6 flex-col items-center">
+            <h4 className="flex text-4xl justify-center underline">Combos</h4>
+            <br></br>
+            <p>
+              <strong>10 plants</strong> - 1 extra point + 1 additional point
+              for each extra plant
+            </p>
+            <br></br>
+            <p>
+              <strong>5 ribbons</strong> - 1 extra point + 1 additional point
+              for each extra ribbon
+            </p>
+            <br></br>
+            <p>
+              <strong>5 animals</strong> - 1 extra point + 1 additional point
+              for each extra animal
+            </p>
+            <br></br>
+            <p>
+              <strong>3 specials (without the rainman)</strong> - 6 extra points
+            </p>
+            <br></br>
+            <p>
+              <strong>4 specials (with the rainman)</strong> - 7 extra points
+            </p>
+            <br></br>
+            <p>
+              <strong>4 specials (without the rainman)</strong> - 8 extra points
+            </p>
+            <br></br>
+            <p>
+              <strong>All 5 specials</strong> - 8 extra points
+            </p>
+            <br></br>
+            <p>
+              <strong>4 of a kind</strong> - 4 extra points, all 4 must
+              represent matching plant type. example:
+              <br></br>
+              <br></br>
+            </p>
+            <ul className="grid h-min w-4/6 grid-cols-4 gap-2">
+              {aprils.map((april) => (
+                <li className="" key={april.id}>
+                  <img
+                    className="h-auto w-auto"
+                    alt="hanafuda-card"
+                    src={april.image}
+                  ></img>
+                </li>
+              ))}
+            </ul>
+            <br></br>
+          </div>
         </div>
       )}
     </div>
